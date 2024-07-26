c80b4e29c96bac88549369ff1a3c36258c7b368d
SandunNawagamuwa-SN <dreamingphoenix916@gmail.com>
2024-07-26 11:18:00 +0530

    implemented Use Register, Login , Logout

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InsurancePolicyController;
+use App\Http\Controllers\AuthController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 Route::apiResource('insurancePolicies', InsurancePolicyController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
