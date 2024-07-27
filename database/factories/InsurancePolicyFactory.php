042757dcab97754b98b431afe4d08daa47bb1003
SandunNawagamuwa-SN <dreamingphoenix916@gmail.com>
2024-07-27 16:09:12 +0530

    Insurance Policy Factory Modified for Run Tests

--- database/factories/InsurancePolicyFactory.php
+++ database/factories/InsurancePolicyFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,11 @@ class InsurancePolicyFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'policy_number' => fake()->unique()->e164PhoneNumber(),
+            'holder_name' => fake()->name(),
+            'type_of_insurance' => fake()->randomElement(['TERM', 'WHOLE', 'UNIVERSAL']),
+            'coverage_amount' => fake()->randomFloat(2, 100, 10000),
         ];
     }
 }
